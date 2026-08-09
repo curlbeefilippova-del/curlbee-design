39e7812ef55e623a455f42d6a1f24fa10f49e3bd
curlbeefilippova-del <curlbeefilippova@gmail.com>
2026-08-09 21:11:21 +0700

    SEO foundation: add app/robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from "next";
+import { SITE_URL } from "./seo";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
+}
+
