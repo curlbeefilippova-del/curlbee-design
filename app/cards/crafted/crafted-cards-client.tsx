840e093b487d0466d55c4f4c722ebc0b6ca8d2cd
curlbeefilippova-del <curlbeefilippova@gmail.com>
2026-08-11 16:48:06 +0700

    Repair language sync source in crafted

--- app/cards/crafted/crafted-cards-client.tsx
+++ app/cards/crafted/crafted-cards-client.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Fragment, useEffect, useRef, useState } from "react";
-import { useCardSwap } from "../use-card-swap";\nimport { useLanguageQuerySync } from "../use-language-query";
+import { useCardSwap } from "../use-card-swap";
+import { useLanguageQuerySync } from "../use-language-query";
 import { typographicCopy, typographicText } from "../../typography";
 
 export type CraftedCardsLanguage = "RU" | "EN";
@@ -58,7 +59,8 @@ const copy = {
 
 export default function CraftedCardsClient({ initialLanguage }: { initialLanguage: CraftedCardsLanguage }) {
   const [language, setLanguage] = useState<CraftedCardsLanguage>(initialLanguage);
-  useLanguageQuerySync(setLanguage);\n  const {
+  useLanguageQuerySync(setLanguage);
+  const {
     activeIndex: activeCard,
     previousIndex: previousCardIndex,
     selectIndex: selectCard,
