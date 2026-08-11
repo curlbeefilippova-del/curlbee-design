695eb917c38bb8caa4d08e33a740990c9f103ba4
curlbeefilippova-del <curlbeefilippova@gmail.com>
2026-08-11 16:33:44 +0700

    Preserve language in card worlds

--- app/cards/crafted/crafted-cards-client.tsx
+++ app/cards/crafted/crafted-cards-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Fragment, useEffect, useRef, useState } from "react";
-import { useCardSwap } from "../use-card-swap";
+import { useCardSwap } from "../use-card-swap";\nimport { useLanguageQuerySync } from "../use-language-query";
 import { typographicCopy, typographicText } from "../../typography";
 
 export type CraftedCardsLanguage = "RU" | "EN";
@@ -58,7 +58,7 @@ const copy = {
 
 export default function CraftedCardsClient({ initialLanguage }: { initialLanguage: CraftedCardsLanguage }) {
   const [language, setLanguage] = useState<CraftedCardsLanguage>(initialLanguage);
-  const {
+  useLanguageQuerySync(setLanguage);\n  const {
     activeIndex: activeCard,
     previousIndex: previousCardIndex,
     selectIndex: selectCard,
